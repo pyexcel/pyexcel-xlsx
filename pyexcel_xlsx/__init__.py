b5b24219f7ddfe3bee9036e3d925d1252534096f
chfw <wangc_2011@hotmail.com>
2016-05-04 23:37:45 +0100

    draft implementation of auto import of pyexcel-xlsx

--- pyexcel_xlsx/__init__.py
+++ pyexcel_xlsx/__init__.py
@@ -10,6 +10,9 @@
 from pyexcel_io.io import get_data as read_data, isstream, store_data as write_data
 
 
+__pyexcel_io_plugins__ = ['xlsx']
+
+
 def save_data(afile, data, file_type=None, **keywords):
     if isstream(afile) and file_type is None:
         file_type='xlsx'
