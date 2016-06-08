578d45e5ccdf709d0937f87e7ae6ca0109b87b5f
chfw <wangc_2011@hotmail.com>
2016-06-08 23:23:27 +0100

    clean code

--- .moban.d/setup.py
+++ .moban.d/setup.py
@@ -1,12 +1,12 @@
 {% extends 'setup.py.jj2' %}
 {%block additional_keywords%}
-        'xlsx'
+    'xlsx'
 {%endblock%}
 {%block additional_classifiers %}
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: PyPy'
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: PyPy'
 {%endblock %}
