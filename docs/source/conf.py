1255c972ae4dcb3c07c76598c96a6d46d3cf7473
chfw <wangc_2011@hotmail.com>
2016-08-31 18:23:31 +0100

    prepeare for 0.2.2 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,8 @@ master_doc = 'index'
 
 project = u'pyexcel-xlsx'
 copyright = u'2015-2016 Onni Software Ltd.'
-version = '0.2.1'
-release = '0.2.2'
+version = '0.2.2'
+release = '0.2.3'
 exclude_patterns = []
 pygments_style = 'sphinx'
 html_theme = 'default'
