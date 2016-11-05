96d8ad97ba9701cd6f77a6559b6ab421b3082a0f
chfw <wangc_2011@hotmail.com>
2016-11-05 21:09:29 +0000

    sync with pyexcel-commons

--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
 
+
 pip freeze
-nosetests --with-cov --cover-package pyexcel_xlsx --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_xlsx  && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
+nosetests --with-cov --cover-package pyexcel_xlsx --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst docs/source pyexcel_xlsx && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
