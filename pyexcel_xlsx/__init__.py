7b2f0dd4c837cd997b178609e8d14d48f7d32be3
chfw <wangc_2011@hotmail.com>
2017-04-06 23:03:25 +0100

    update plugin protocol

--- pyexcel_xlsx/__init__.py
+++ pyexcel_xlsx/__init__.py
@@ -12,6 +12,7 @@
 # because of dynamic import
 __FILE_TYPE__ = 'xlsx'
 __META__ = {
+    'plugin_type': 'pyexcel io plugin',
     'submodule': __FILE_TYPE__,
     'file_types': [__FILE_TYPE__, 'xlsm'],
     'stream_type': 'binary'
