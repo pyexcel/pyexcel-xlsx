45346d5a162ea4fbdbd537849ff224706ffd2fce
chfw <wangc_2011@hotmail.com>
2017-07-16 15:32:11 +0100

    release code refactored version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ master_doc = 'index'
 
 project = u'pyexcel-xlsx'
 copyright = u'2015-2017 Onni Software Ltd.'
-version = '0.4.0'
+version = '0.4.1'
 release = '0.4.1'
 exclude_patterns = []
 pygments_style = 'sphinx'
