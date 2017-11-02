cbc62965cc5cf355bf72f1642ce89b9782ca054b
chfw <wangc_2011@hotmail.com>
2017-11-02 08:48:01 +0000

    :umbrella: test complex hidden sheets, #20

--- tests/test_hidden.py
+++ tests/test_hidden.py
@@ -1,10 +1,20 @@
 import os
 from nose.tools import eq_
-from pyexcel_xls import get_data
+from pyexcel_xlsx import get_data
 
 
 def test_hidden_row():
     data = get_data(os.path.join("tests", "fixtures", "hidden.xlsx"),
-                    skip_hidden_row_and_column=True)
+                    skip_hidden_row_and_column=True,
+                    library='pyexcel-xlsx')
     expected = [[1, 2], [7, 9]]
+    eq_(data['Sheet1'], expected)
+
+
+def test_complex_hidden_sheets():
+    data = get_data(
+        os.path.join("tests", "fixtures", "complex_hidden_sheets.xlsx"),
+        skip_hidden_row_and_column=True,
+        library='pyexcel-xlsx')
+    expected = [[1, 3, 5, 7, 9], [31, 33, 35, 37, 39], [61, 63, 65, 67]]
     eq_(data['Sheet1'], expected)
