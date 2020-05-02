f6ca4cb8ccd2fe446af78f17dc77cb50c01e843f
chfw <wangc_2011@hotmail.com>
2020-05-02 23:42:01 +0100

    :books: update project meta data

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-# Template by pypi-mobans
+"""
+Template by pypi-mobans
+"""
+
+import os
+import sys
 import codecs
 import locale
-import os
 import platform
-import sys
 from shutil import rmtree
 
-from setuptools import Command, find_packages, setup
+from setuptools import Command, setup, find_packages
 
 
 # Work around mbcs bug in distutils.
@@ -51,13 +54,12 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
-
     "Programming Language :: Python :: 3.7",
-
     "Programming Language :: Python :: 3.8",
 
     'Programming Language :: Python :: Implementation :: PyPy'
 ]
+
 
 INSTALL_REQUIRES = [
     "openpyxl>=2.6.1",
@@ -65,8 +67,7 @@ INSTALL_REQUIRES = [
 ]
 SETUP_COMMANDS = {}
 
-
-PACKAGES = find_packages(exclude=["ez_setup", "examples", "tests"])
+PACKAGES = find_packages(exclude=["ez_setup", "examples", "tests", "tests.*"])
 EXTRAS_REQUIRE = {
 }
 # You do not need to read beyond this line
