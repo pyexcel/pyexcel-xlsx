0524e27b264ff606e82aeb5244939a52c1cd2668
chfw <wangc_2011@hotmail.com>
2025-05-03 12:19:45 +0100

    :sparkles: test append two dimensinal array as a new sheet. #56

--- tests/test_book.py
+++ tests/test_book.py
@@ -32,3 +32,17 @@ def test_create_new_sheet():
     data = get_data(test_file)
     eq_(data["alien"], [["Test"]])
     os.unlink(test_file)
+
+
+def test_create_append_new_data():
+    test_file = "book_test.xlsx"
+    book = Book(os.path.join("tests", "fixtures", "date_field.xlsx"))
+    sheet = book["alien"]
+    sheet.append([1, 2, 3])
+    sheet.append([3, 4, 5])
+    book.save(test_file)
+    book.close()
+
+    data = get_data(test_file)
+    eq_(data["alien"], [[1, 2, 3], [3, 4, 5]])
+    os.unlink(test_file)
