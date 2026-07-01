43c5ebbd0538672c60a8c6821c099e85712bcad0
chfw <wangc_2011@hotmail.com>
2026-07-01 21:52:28 +0100

    update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 pip freeze
-nosetests --with-coverage --cover-package pyexcel_xlsx --cover-package tests tests --with-doctest --doctest-extension=.rst README.rst docs/source pyexcel_xlsx
+coverage run -m --source=pyexcel_xlsx pytest --doctest-modules && coverage report --show-missing
